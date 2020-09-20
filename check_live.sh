7d04aad1b8793c0cb0874b11de466053897ac388
ax3de0s <57349753+ax3de0s@users.noreply.github.com>
2020-09-20 17:14:09 +0000

    check_live.sh
    update ip range

--- check_live.sh
+++ check_live.sh
@@ -3,7 +3,7 @@
 echo -n "Please enter a target IP address: "
 read TARGET_IP
 
-sudo masscan -p1-100,U:1-100 $TARGET_IP --rate=1000 -e tun0 -oL ips.txt | awk '{ print $3}' | sort -u > live_ips.txt
+sudo masscan -p1-65535,U:1-65535 $TARGET_IP --rate=1000 -e tun0 -oL ips.txt | awk '{ print $3}' | sort -u > live_ips.txt
 
 done
 exit 0
